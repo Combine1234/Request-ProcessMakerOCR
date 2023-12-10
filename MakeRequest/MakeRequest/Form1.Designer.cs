36c513b755a5032fe1d114fca97c95c5e1a19411
Uncle Fronk <79006075+Combine1234@users.noreply.github.com>
2023-12-10 15:01:59 +0700

    Add files via upload

---
+++ MakeRequest/MakeRequest/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace MakeRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(23, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(770, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Write";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tray :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pos  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Return :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(150, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(638, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(150, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(638, 34);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(150, 161);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(638, 34);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Write Request";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(207, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Read Request";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(23, 390);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(335, 277);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(365, 390);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Read Line 1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(365, 448);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Read Line 2:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(365, 501);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Read Line 3:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(365, 553);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 16);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Read Line 4:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(368, 411);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(420, 30);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(368, 467);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(420, 30);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(368, 520);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(420, 30);
+            this.textBox7.TabIndex = 16;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(368, 572);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(420, 30);
+            this.textBox8.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(368, 611);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(420, 56);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(27, 338);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 33);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(112, 341);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(676, 30);
+            this.textBox9.TabIndex = 20;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox10.Location = new System.Drawing.Point(23, 702);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(765, 30);
+            this.textBox10.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 683);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 16);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Result";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 753);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Write&RequestMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MakeRequest/MakeRequest/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
+
+namespace MakeRequest
+{
+    public partial class Form1 : Form
+    {
+        private Queue<string> fileQueue = new Queue<string>();
+        public string Folder;
+        public string newtext1;
+        public string newtext2;
+        public string sourceFilePath;
+        private int fileCounter = 1;
+        private int fileCounter2 = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Construct the file name with the counter
+            string fileName = $"request_{fileCounter.ToString("D2")}.txt";
+
+            StreamWriter A = new StreamWriter(Path.Combine(Application.StartupPath, "request", fileName));
+            A.WriteLine(label1.Text + " " + textBox1.Text);
+            A.WriteLine(label2.Text + " " + textBox2.Text);
+            A.WriteLine(label3.Text);
+            A.Close();
+
+            // Increment the counter for the next file
+            fileCounter++;
+            MessageBox.Show("บันทึกเรียบร้อย");
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                // Construct the file name with the counter
+                string fileName = $"request_{fileCounter.ToString("D2")}.txt";
+
+                StreamWriter A = new StreamWriter(Path.Combine(Application.StartupPath, "request", fileName));
+                A.WriteLine(label1.Text + " " + textBox1.Text);
+                A.WriteLine(label2.Text + " " + textBox2.Text);
+                A.WriteLine(label3.Text);
+                A.Close();
+
+                // Increment the counter for the next file
+                fileCounter++;
+                MessageBox.Show("บันทึกเรียบร้อย");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private string SanitizeFileName(string fileName)
+        {
+            // Remove any illegal characters from the file name
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            return new string(fileName.Where(c => !invalidChars.Contains(c)).ToArray());
+        }
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Folder = folderBrowserDialog1.SelectedPath;
+                textBox9.Text = folderBrowserDialog1.SelectedPath;
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string folderPath = Folder;
+            
+            // Check if the folder exists
+            if (Directory.Exists(folderPath))
+            {
+                // Get all files in the folder
+                string[] files = Directory.GetFiles(folderPath);
+
+                // Add files to the queue
+                foreach (string file in files)
+                {
+                    fileQueue.Enqueue(file);
+
+                    if (File.Exists(file))
+                    {
+                        string textContent = File.ReadAllText(file);
+                        textBox4.Text = textContent;
+
+                        string[] line = File.ReadAllLines(file);
+                        foreach (string l in line)
+                        {
+                            if (l.Contains("Tray"))
+                            {
+                                newtext1 = l.Replace("Tray : ", "");
+                                textBox5.Text = newtext1;
+                            }
+                            if (l.Contains("Pos"))
+                            {
+                                newtext2 = l.Replace("Pos  : ", "");
+                                textBox6.Text = newtext2;
+                            }
+                            if (l.Contains("Return"))
+                            {
+                                textBox7.Text = l;
+                            }
+
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(newtext1) && !string.IsNullOrEmpty(newtext2))
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(@"D:\", "HolyOCR")); // Replace with your actual directory path
+
+                        if (directoryInfo.Exists)
+                        {
+                            // Search for files containing both newtext1 and newtext2
+                            FileInfo[] fils = directoryInfo.GetFiles($"*{newtext1}*{newtext2}*");
+
+                            if (fils.Length > 0)
+                            {
+                                StringBuilder fileNames = new StringBuilder();
+
+                                foreach (FileInfo fileInfo in fils)
+                                {
+                                    fileNames.AppendLine(fileInfo.FullName);
+                                }
+                                sourceFilePath = fileNames.ToString();
+                                textBox10.Text = fileNames.ToString();
+                            }
+                            else
+                            {
+                                textBox10.Text = $"No files found in {directoryInfo.FullName} containing both {newtext1} and {newtext2}.";
+                            }
+                        }
+                        else
+                        {
+                            textBox10.Text = "Directory does not exist.";
+                        }
+                    }
+                    else
+                    {
+                        textBox10.Text = "Specify both search terms first.";
+                    }
+                    string fileName = $"nissan_{fileCounter2.ToString("D2")}.txt";
+
+                    StreamWriter A = new StreamWriter(Path.Combine(Application.StartupPath, "nissan", fileName));
+                    A.WriteLine(label1.Text + " " + textBox5.Text);
+                    A.WriteLine(label2.Text + " " + textBox6.Text);
+                    A.WriteLine(label3.Text);
+                    A.Close();
+
+                    // Increment the counter for the next file
+                    fileCounter2++;
+                    string filedel = file;
+                    File.Delete(filedel);
+                    try 
+                    {
+                        string destinationFolderPath = @"C:\Users\Fronk1221\source\repos\MakeRequest\MakeRequest\bin\Debug\targetPic\" + textBox5.Text +"_"+ textBox6.Text + ".jpg";
+                        File.Copy(sourceFilePath, destinationFolderPath);
+                    }
+                    catch 
+                    {
+                        return;
+                    }
+
+                    DisplayNextFile();
+                }
+
+                // Display the files in the TextBox
+            }
+            else
+            {
+                MessageBox.Show("The specified folder does not exist.");
+                timer1.Stop();
+            }
+        }
+
+        private void DisplayNextFile()
+        {
+            if (fileQueue.Count > 0)
+            {
+                // Dequeue the next file from the queue
+                string nextFile = fileQueue.Dequeue();
+
+                // Display the file in the TextBox
+                textBox1.AppendText(nextFile + Environment.NewLine);
+
+                // Optionally, you can call this method recursively to display all files
+                // DisplayNextFile();
+            }
+            else
+            {
+                MessageBox.Show("No more files in the queue.");
+            }
+        }
+        private void folderBrowserDialog1_HelpRequest_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
